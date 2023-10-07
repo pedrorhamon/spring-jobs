b97bcf5c24772a35e8109fc67959302d8e4eaa36
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-07 18:56:44 -0300

    criando metodo starBatch para execucao

--- src/main/java/com/starking/batch/configuration/BatchConfigurer.java
+++ src/main/java/com/starking/batch/configuration/BatchConfigurer.java
@@ -1,7 +1,10 @@
 package com.starking.batch.configuration;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +16,11 @@ import org.springframework.context.annotation.Configuration;
 public class BatchConfigurer  extends DefaultBatchConfiguration {
 	
 	@Bean
-	public Job startBatch() {
-		
+	public Job startBatch(JobBuilderFactory jobBuilderFactory, Step step1) {
+		return jobBuilderFactory.get("contractEffective")
+				.incrementer(new RunIdIncrementer())
+				.start(step1)
+				.build();
 	}
 
 }
