d8d7d9f8d80b3e09ed5ff48cd25a390031b99dfe
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-08 17:44:04 -0300

    criando step para o batch

--- src/main/java/com/starking/batch/configuration/BatchConfigurer.java
+++ src/main/java/com/starking/batch/configuration/BatchConfigurer.java
@@ -3,10 +3,17 @@ package com.starking.batch.configuration;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.starking.batch.model.Contract;
+import com.starking.batch.model.ContractHistory;
 
 /**
  * @author pedroRhamon
@@ -21,6 +28,19 @@ public class BatchConfigurer  extends DefaultBatchConfiguration {
 		return jobBuilderFactory.get("contractEffective")
 				.incrementer(new RunIdIncrementer())
 				.start(step1)
+				.build();
+	}
+	
+	@Bean
+	public Step step1(StepBuilderFactory stepBuilderFactory,
+			ItemReader<Contract> itemReader,
+            ItemProcessor<Contract, ContractHistory> itemProcessor,
+            ItemWriter<ContractHistory> itemWriter) {
+		return stepBuilderFactory.get("step1")
+				.<Contract, ContractHistory>chunk(1000)
+				.reader(itemReader)
+				.processor(itemProcessor)
+				.writer(itemWriter)
 				.build();
 	}
 
