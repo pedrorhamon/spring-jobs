d9d0a3a39d84f7d61a5117f3f1da66aefbdae30f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-08 17:55:07 -0300

    adicionando anotações

--- src/main/java/com/starking/batch/configuration/ItemWriterConfiguration.java
+++ src/main/java/com/starking/batch/configuration/ItemWriterConfiguration.java
@@ -1,8 +1,15 @@
 package com.starking.batch.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author pedroRhamon
  */
+
+@Configuration
+@Slf4j
 public class ItemWriterConfiguration {
 
 }
